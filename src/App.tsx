503eac6a4ed115a82d239df6c5f69bcee53e221f
jitka.maresova <jitka.maresova@dnai.ai>
2023-06-30 19:43:26 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Learn 27 ports with our Ports App</h1>
+      <div className="starter">
+        <img src="ports.png" alt="ports" className="starter__image" />
+        <button className="starter__btn">Get started</button>
+      </div>
+    </div>
+  );
+};
