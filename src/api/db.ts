680e8c4f47af90f4455de3ddde773b58e29c4a9f
jitka.maresova <jitka.maresova@dnai.ai>
2023-07-02 12:07:00 +0200

    supabase

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,6 +1,6 @@
 import { supabase } from "./supabase";
 
 export const getPortsData = async () => {
-  const { data: ports, error } = await supabase.from("ports").select("*");
-  return ports;
+  const { data } = await supabase.from("ports").select("*");
+  return data;
 };
